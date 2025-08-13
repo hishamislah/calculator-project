eb14d4ef52deda10dd05bffa4e362c5fa0aec4d4
Hisham Islah <hishamislah2003@gmail.com>
2025-08-13 13:00:21 +0530

    Add addition and subtraction functions

--- calculator.py
+++ calculator.py
@@ -1,8 +1,12 @@
 # calculator.py
 
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
 def main():
     print("Simple Calculator")
-    print("This will have addition, subtraction, multiplication, division")
-
-if __name__ == "__main__":
-    main()
+    print("Addition: 5 + 3 =", add(5, 3))
+    print("Subtraction: 5 - 3 =", subtract(5, 3))
